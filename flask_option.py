10ab2d06b57ad5e0fbd3df2f676cb4a1aef3c18d
Semih Yildirim <semih.yildirim@marun.edu.tr>
2026-02-10 23:43:56 +0300

    Add start function with FLASK_ENV environment variable

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,12 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    flask_env = os.getenv("FLASK_ENV")
+    if flask_env == "development":
+        return "Starting in development mode..."
+    if flask_env == "production":
+        return "Starting in production mode..."
+    return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
